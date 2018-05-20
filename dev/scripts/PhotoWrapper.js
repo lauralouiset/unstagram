bf19c9db6ecc6460697a0c165a1f4621e3adc5f9
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-05-20 17:18:44 -0400

    Photo input workinggit add -A

--- dev/scripts/PhotoWrapper.js
+++ dev/scripts/PhotoWrapper.js
@@ -10,84 +10,69 @@ class PhotoWrapper extends React.Component {
 
 	constructor(props){
 		super();
+		this.validTypes = ['image/png', 'image/jpg', 'image/gif', 'image/jpeg'];
 		this.state = {
 			photoFile: '',
 			canvasWidth: 50,
 			canvasHeight: 50,
-				effect: function(){},
+			effect: function(){},
 			data : '',
 		}
-		// this.overlay = this.overlay.bind(this);
-		this.updatePhotoFile = this.updatePhotoFile.bind(this);
+		this.overlay = this.overlay.bind(this);
+		this.inputPhoto = this.inputPhoto.bind(this);
+		this.displayPhoto = this.displayPhoto(this);
+	}
+	isValidFile(data) {
+		return (this.validTypes.indexOf(data) !== -1) ? true : false;
+	}
+	inputPhoto(e){
+		const file = e.target.files[0];
+		console.log(file);
+		if (this.isValidFile(file.type)){
+			const reader = new FileReader();
+			reader.addEventListener("load", () => {
+				this.displayPhoto(reader.result);
+				});
+			reader.readAsDataURL(file);
+		} else {
+			alert("Invalid file type!");
+		}
+		this.setState({
+			photoFile: file
+		})
 	}
 
-	updatePhotoFile(e) {
-		// get file from input
-		const newFile = e.target.files[0];
-		let fileName = "";
-		console.log(fileName);
-		// init FileReader API
-		const reader = new FileReader();
-		// if a file is entered, set fileName and
-		// get data.
-		if (newFile) {
-			fileName = newFile.name;
-			reader.readAsDataURL(newFile);
-			let imageData = reader.result;
-			console.log(imageData);
-		}
-		let img = new Image();
-		img.src = this.state.imageData;
-
-		this.setState({
-			photoFile : fileName,
-			value: e.target.value,
-			canvasWidth : img.width,
-			canvasHeight : img.height
-		})
-
-		// display image on canvas
+	displayPhoto(data){
 		const canvas = ReactDOM.findDOMNode(this.refs.myCanvas);
 		const ctx = canvas.getContext('2d');
+		let img = new Image();
+		img.src = data;
 
-		img.onload = insertPhoto;
-		function insertPhoto() {
-			let image = new Image();
-			// Set image src
-			image.src = this.state.imageData;
-			console.log(this.state.imageData);
-			// On image load add to canvas
-			// img.onload = function () {
-			// 	canvas.width = this.state.canvasWidth;
-			// 	canvas.height = this.state.canvasHeight;
-			// 	ctx.drawImage(img, 0, 0, this.state.canvasWidth, this.state.canvasHeight);
-			// }
-		}
+		img.onload = () => {
+			canvas.width = img.width;
+			canvas.height = img.height;
+			ctx.drawImage(img, 0, 0, img.width, img.height);
+			}
 	}
-
-	componentDidMount(){
-	}
-
 	render(){
 		return(
 		<div className="PhotoWrapper">
 			<div className="photoContainer">
 				<div id="canvasContainer">
-					<canvas ref="myCanvas"></canvas>
+						<canvas ref="myCanvas"></canvas>
 			</div>
 
 				<div className="photoButtons">
-						<form onSubmit={this.submitPhoto}>
-							<label htmlFor="fileInput">Upload Photo</label>
-							<input type="file" name="fileInput" onChange={this.props.updatePhotoFile} />
-						</form>
-
+					<label htmlFor="fileInput"></label>
+					<input type="file" name="fileInput" onChange={this.inputPhoto} />
 					<button type="button"className="btn--submit">Save Photo</button>
 
 				</div>
 			</div>
 
-			<Filters />
+			<Filters 
+			overlay = {this.overlay}
+			/>
 
 		</div>
 		)
